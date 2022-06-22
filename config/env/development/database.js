927a78d64811bb887300352ba3362730583221e1
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2022-06-22 21:07:45 +0530

    Updated config

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: 'postgres://acprfqdnelxhyo:7e2b7f31a4e963e3a99c62043bbc65a16366348056c8a05bb49d525a43fc3461@ec2-34-200-35-222.compute-1.amazonaws.com:5432/dbboaqbl2lsu8m',
       port: config.port,
-      database: config.database,
+      database: 'postgres://acprfqdnelxhyo:7e2b7f31a4e963e3a99c62043bbc65a16366348056c8a05bb49d525a43fc3461@ec2-34-200-35-222.compute-1.amazonaws.com:5432/dbboaqbl2lsu8m',
       user: config.user,
       password: config.password,
       ssl: {
