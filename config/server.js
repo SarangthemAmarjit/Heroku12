675df2905eb74ab947a7d1c36883c66dc77625d7
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2022-06-22 23:34:55 +0530

    Updated config

--- config/server.js
+++ config/server.js
@@ -1,12 +1,13 @@
 module.exports = ({ env }) => ({
 host: env('HOST', '0.0.0.0'),
 port: env.int('PORT', 1337),
+app: {
+    keys: env.array('APP_KEYS'),
+  },
 autoReload: { 'enabled': true } , admin: {
 auth: {
 secret: env('ADMIN_JWT_SECRET', 'yFnJKphbMeSKbgGW7XanRg=='),
 },
-app: {
-    keys: env.array('APP_KEYS'),
-  },
+
 },
 });
