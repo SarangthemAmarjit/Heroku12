25e74e265098d02216ee03606088c2000aecb2f5
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2022-06-22 21:38:08 +0530

    Updated config

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -5,15 +5,15 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: '0.0.0.0',
+      port: '1337',
+      database: 'postgres://acprfqdnelxhyo:7e2b7f31a4e963e3a99c62043bbc65a16366348056c8a05bb49d525a43fc3461@ec2-34-200-35-222.compute-1.amazonaws.com:5432/dbboaqbl2lsu8m',
+      user: 'sarangthemamarjit123@gmail.com',
+      password: 'Langthabal123@',
       ssl: {
         rejectUnauthorized: false
       },
     },
-    debug: false,
+    debug: true,
   },
 });
